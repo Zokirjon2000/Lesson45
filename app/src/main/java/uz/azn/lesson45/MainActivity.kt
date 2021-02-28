ecf2ac44bc0ccce7f17110af7bbb2ff164097f7c
AZNAndroidDeveloper <zokirjonashiraliyev@bk.ru>
2021-02-28 17:36:53 +0500

    my first commit

---
+++ app/src/main/java/uz/azn/lesson45/MainActivity.kt
@@ -0,0 +1,12 @@
+package uz.azn.lesson45
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uz.azn.mylibrary.MyMath
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ mylibrary/src/main/java/uz/azn/mylibrary/MyMath.kt
@@ -0,0 +1,11 @@
+package uz.azn.mylibrary
+
+class MyMath {
+    companion object{
+        fun plusNumber(a:Int,b:Int):Int = a+b
+        fun minusNumber(a:Int,b:Int):Int = a-b
+        fun multiplyNumber(a:Int,b:Int):Int = a*b
+        fun divisionNumber(a:Int,b:Int):Int = a/b
+
+    }
+}
